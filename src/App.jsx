f97a8098ae469d565050c23901fe5151971a5b93
Eduardo-Muricy <Eduardo-Muricy@hotmail.com>
2025-05-22 22:08:09 -0300

    Enviando dados HTTP

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './App.css'
+
+
+const url = 'http://localhost:3000/products'
+
+function App() {
+  const [products, setProducts] = useState([])
+
+
+
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+
+    getData()
+  }, [])
+
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+      const res = await fetch(url,{
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(product)
+      })
+      console.log(res)
+  }
+
+
+
+  return (
+    <div>
+      {products.map((product) => (
+        <ul key={product.map}>
+          <li> Produto:{product.name}</li>
+          <li>preço: R${product.price * 5.20}</li>
+        </ul>
+      ))}
+
+      <div  className='form-container'>
+        <form onSubmit={handleSubmit}>
+
+          <input type="text" placeholder='nome' value={name} onChange={(e) => setName(e.target.value)} />
+
+          <input type="text" placeholder='preco' value={price} onChange={(e) => setPrice(e.target.value)} />
+
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
